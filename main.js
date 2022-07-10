02d5f2c76d362d00f91246f7cd4463f7bc751172
Mina McGonigal <mcgonigal.mina@gmail.com>
2022-07-10 01:09:06 -0500

    navbar js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 'use strict'
+
+
+
+//make navbar transparent when it is on the top. else will have background color;
+const navbar=document.querySelector("#navbar");
+const navbarHeight=navbar.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+    console.log(window.scrollY);
+    console.log(`"navbarHeight: ${navbarHeight}`);
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add("navbar_dark");
+    }else{
+        navbar.classList.remove("navbar_dark")
+    }
+
+})
